f7590c9cc19b46cc1985bc86bcd2ba57d9c2e2b3
cspangler <connor.spangler.1@us.af.mil>
2018-11-14 00:36:05 -0600

    ID'd case w/ external link content

--- src/smparser.py
+++ src/smparser.py
@@ -27,7 +27,10 @@ for fbu in facebook_unzips:
     comments = json.loads(comments_json)['comments']
     for comment in comments:
         timestamp = datetime.fromtimestamp(comment['timestamp'], timezone.utc)
+        if 'attachments' in comment:
+            print('FOUND ATTACHMENT: {0}'.format(comment['attachments'][0]['data'][0]['external_context']['url']))
         comment_date = timestamp.date()
         comment_time = timestamp.strftime("%#I:%M %p") if platform.system() == 'Windows' else timestamp.strftime("%-I:%M %p")
-        print('{0} @ {1}'.format(comment_date, comment_time))
+        #print(comment['data'][0]['comment']['comment'] or '### NO COMMENT ###')
+        #print('{0} @ {1}'.format(comment_date, comment_time))
 
