4e3dd5c7ad863c109dc27b1179c06d12b2d37f32
cspangler <connor.spangler.1@us.af.mil>
2018-11-13 23:18:21 -0600

    Added parsing of unzipped dataset locations

--- src/smparser.py
+++ src/smparser.py
@@ -1,8 +1,18 @@
 import zipfile
 import glob
+import os
+import re
+import sys
 
 # Parse Facebook files
-facebook_zips = glob.glob('./inbox/*_facebook.zip')
-for fbz in facebook_zips:
-    with zipfile.ZipFile(fbz,"r") as zip_ref:
-        zip_ref.extractall("./inbox/temp")
+#facebook_zips = glob.glob('./inbox/*_facebook.zip')
+#for fbz in facebook_zips:
+#    with zipfile.ZipFile(fbz,"r") as zip_ref:
+#        zip_ref.extractall("./inbox/temp")
+
+if len(sys.argv) != 2:
+    sys.exit("ERROR: Path to zips required")
+unzips = os.listdir('./src/inbox/temp')
+fb_regex = re.compile(r'.*_facebook$')
+facebook_unzips = list(filter(fb_regex.search, unzips))
+print(facebook_unzips)
