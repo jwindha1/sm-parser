01eb8cf50722880cf3f6371fae6f990899518289
Connor Spangler <connormspangler@gmail.com>
2018-11-13 23:38:54 -0600

    Successfully parsing out individual comments

--- src/smparser.py
+++ src/smparser.py
@@ -3,6 +3,7 @@ import glob
 import os
 import re
 import sys
+import json
 
 # Parse Facebook files
 #facebook_zips = glob.glob('./inbox/*_facebook.zip')
@@ -10,9 +11,16 @@ import sys
 #    with zipfile.ZipFile(fbz,"r") as zip_ref:
 #        zip_ref.extractall("./inbox/temp")
 
+temp_out = os.path.join('src', 'inbox', 'temp')
+
 if len(sys.argv) != 2:
     sys.exit("ERROR: Path to zips required")
-unzips = os.listdir('./src/inbox/temp')
+unzips = os.listdir(temp_out)
 fb_regex = re.compile(r'.*_facebook$')
 facebook_unzips = list(filter(fb_regex.search, unzips))
-print(facebook_unzips)
+
+for fbu in facebook_unzips:
+    comments_path = os.path.join(temp_out, fbu, 'comments', 'comments.json')
+    comments_json = open(comments_path).read()
+    comments = json.loads(comments_json)['comments']
+    print(len(comments))
